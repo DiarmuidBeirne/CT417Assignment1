a9a134d765cfc07ca190405eb89dd0093a5a95e3
Diarmuid Beirne <d.beirne.8@gmail.com>
2018-10-23 15:10:10 +0100

    Started CourseTest

--- src/test/java/testPackage/CourseTest.java
+++ src/test/java/testPackage/CourseTest.java
@@ -7,8 +7,18 @@ Author: 15331436 | Diarmuid Beirne
 23 Oct 2018
 */
 
+import com.diarmuidbeirne.ct417assignment1.Course;
+import com.diarmuidbeirne.ct417assignment1.Module;
+import org.joda.time.LocalDate;
+
+import java.util.ArrayList;
+
 public class CourseTest {
 
+    ArrayList<Module> moduleList = new ArrayList<>();
+    LocalDate testStartDate = new LocalDate(2018,06,10);
+    LocalDate testEndDate = new LocalDate(2019,01,01);
+    private Course testCourse = new Course("Test Course", moduleList,testStartDate,testEndDate);
     public CourseTest() {}
 
 }
